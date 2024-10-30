44b8f299d027a79e46c88be167dcd272c10c0bab
bain-liao <liao.bain@mths.ca>
2024-10-30 11:27:53 -0400

    created motor program

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Oct 2024
+ * This program moves a stepper motor back and forth
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+let objectDistance: number = 0
+
+// loop forever
+while (true) {
+    if (input.buttonIsPressed(Button.A) == true) {
+        objectDistance = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+    )
+        // move the motor forwards
+        robotbit.StpCarMove(10, 48)
+        basic.showIcon(IconNames.Happy)
+    }
+
+    if (objectDistance < 10) {
+        // move car forwards and backwards
+        robotbit.StpCarMove(-10, 48)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        basic.showIcon(IconNames.Happy)
+    }
+}

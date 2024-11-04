d1274b11506b83dc52d1a2bccea9e084db65511f
bain-liao <liao.bain@mths.ca>
2024-11-04 11:49:58 -0500

    updated program

--- main.ts
+++ main.ts
@@ -10,23 +10,24 @@ basic.showIcon(IconNames.Happy)
 let objectDistance: number = 0
 
 // loop forever
-while (true) {
+    while (true) {
     if (input.buttonIsPressed(Button.A) == true) {
+        // find the distance
         objectDistance = sonar.ping(
             DigitalPin.P1,
             DigitalPin.P2,
             PingUnit.Centimeters
     )
-        // move the motor forwards
-        robotbit.StpCarMove(10, 48)
-        basic.showIcon(IconNames.Happy)
     }
-
-    if (objectDistance < 10) {
-        // move car forwards and backwards
-        robotbit.StpCarMove(-10, 48)
+    if (objectDistance > 10) {
+        // move car forwards
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(48, 65)
+    } else {
+        // move car backwards and turn 90 degrees
+        basic.showIcon(IconNames.No)
         robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
         robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-        basic.showIcon(IconNames.Happy)
+        robotbit.StpCarMove(-10, 65)
+        }
     }
-}

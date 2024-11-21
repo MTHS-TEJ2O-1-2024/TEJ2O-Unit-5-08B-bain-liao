b39dfe45a3a2d9f4920f55b1ec68c01a6279d250
bain-liao <liao.bain@mths.ca>
2024-11-20 19:44:44 -0500

    updated code to test if this loops forever

--- main.ts
+++ main.ts
@@ -11,32 +11,32 @@ let objectDistance: number = 0
 
 // loop forever
     while (true) {
-    if (input.buttonIsPressed(Button.A) == true) {
+    if (input.buttonIsPressed(Button.A)) {
         // find the distance
         objectDistance = sonar.ping(
             DigitalPin.P1,
             DigitalPin.P2,
             PingUnit.Centimeters
-    )
-    // if distance is greater than 10
-    if (objectDistance > 10) {
-        // move car forwards
-        basic.clearScreen()
-        basic.showIcon(IconNames.Yes)
-        robotbit.StpCarMove(10, 48)
-    } else {
-        basic.showIcon(IconNames.No)
-        robotbit.StpCarMove(0, 0)
-        basic.pause(500)
+        )
+        // if distance is greater than 10
+        if (objectDistance > 10) {
+            // move car forwards
+            basic.clearScreen()
+            basic.showIcon(IconNames.Yes)
+            robotbit.StpCarMove(10, 48)
+        } else {
+            basic.showIcon(IconNames.No)
+            robotbit.StpCarMove(0, 0)
+            basic.pause(500)
 
-        // car backing up
-        robotbit.StpCarMove(-10, 48)
-        basic.pause(1000)
+            // car backing up
+            robotbit.StpCarMove(-10, 48)
+            basic.pause(1000)
 
-        // car turning 90 degrees
-        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
-        basic.pause(500)
-        robotbit.StpCarMove(10, 48)
+            // car turning 90 degrees
+            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+            basic.pause(500)
+            robotbit.StpCarMove(10, 48)
         }
     }
 }
